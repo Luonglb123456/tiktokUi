88c54f0ecf93c80f9fa44fcb373372c72286de93
Luonglb123456 <luonglb123@gmail.com>
2024-07-12 10:06:42 +0700

    giải quyết vấn đề nhiều file index.js

---
+++ src/routes/routes.js
@@ -0,0 +1,26 @@
+//Layouts
+import { HeaderOnly } from '~/layouts';
+
+import config from '~/config';
+
+import Home from '~/pages/Home';
+import Following from '~/pages/Following';
+import Profile from '~/pages/Profile';
+import Upload from '~/pages/Upload';
+import Search from '~/pages/Search';
+
+//Public Routes
+const publicRoutes = [
+    { path: config.routes.home, component: Home },
+    { path: config.routes.following, component: Following },
+    { path: config.routes.profile, component: Profile },
+    { path: config.routes.upload, component: Upload, layout: HeaderOnly }, //Layout thằng này chỉ có Header thôi
+    { path: config.routes.search, component: Search, layout: null },
+];
+
+const privateRoutes = [];
+
+export { publicRoutes, privateRoutes };
+
+//routes này chứa các tuyến đường Home thì đi đâu
+// following thì đi đâu
